d0f4b07668359faabccd0840de8a707f91cf0826
Titik Nurfaidah <titik.nurfaidah@1rstwap.com>
2021-03-03 16:19:53 +0700

    link belajar

---
+++ src/main/java/com/amqp/messagingrabbitmq/Receiver.java
@@ -0,0 +1,25 @@
+package com.amqp.messagingrabbitmq;
+
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * https://spring.io/guides/gs/messaging-rabbitmq/#scratch
+ */
+@Component
+public class Receiver
+{
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    public void receiveMessage(String message)
+    {
+        System.out.println("Received <" + message + ">");
+        latch.countDown();
+    }
+
+    public CountDownLatch getLatch()
+    {
+        return latch;
+    }
+}
