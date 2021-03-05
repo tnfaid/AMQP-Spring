a4e23409331d529eeb063cf35f561f54830fed6b
Titik Nurfaidah <titik.nurfaidah@1rstwap.com>
2021-03-05 16:44:10 +0700

    lanjut di rumah bogor

--- src/main/java/com/amqp/messagingrabbitmq/Receiver.java
+++ src/main/java/com/amqp/messagingrabbitmq/Receiver.java
@@ -7,6 +7,11 @@ import java.util.concurrent.CountDownLatch;
 /**
  * https://spring.io/guides/gs/messaging-rabbitmq/#scratch
  * https://www.baeldung.com/spring-amqp-reactive
+ * https://www.baeldung.com/rabbitmq-spring-amqp
+ *
+ * https://stackoverflow.com/questions/43513681/rabbitmq-get-messages-from-a-queue-using-curl
+ * https://cdn.rawgit.com/rabbitmq/rabbitmq-management/v3.7.9/priv/www/api/index.html
+ * https://stackoverflow.com/questions/53351186/publish-to-rabbitmq-queue-with-http-api
  */
 @Component
 public class Receiver
